c69570690bd242784eb517053cb1f5e793ac0a90
Andry Ariadi <andryariadi23@gmail.com>
2024-02-07 06:50:31 +0700

    update favicon

--- i-bike/src/app/layout.jsx
+++ i-bike/src/app/layout.jsx
@@ -1,20 +1,24 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata = {
   title: {
-    default: "iBike",
-    template: "%s | iBike",
+    default: "iBikez",
+    template: "%s | iBikez",
   },
-  description: "andry ariadi iBike e-commerce",
+  description: "andry ariadi iBikez e-commerce",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.variable}>{children}</body>
     </html>
   );
 }
