f0e9c17193eab1a5ecbd1eeb6fe5d9945bc98abf
Andry Ariadi <andryariadi23@gmail.com>
2024-02-09 22:47:58 +0700

    update styling register page

--- i-bike/src/app/(auth)/register/page.jsx
+++ i-bike/src/app/(auth)/register/page.jsx
@@ -1,9 +1,17 @@
 import RegisterForm from "@/components/RegisterForm";
+import Image from "next/image";
 
 export default function RegisterPage() {
   return (
     <>
-      <RegisterForm />
+      <div className="bg-amber-500 flex justify-between items-center gap-20 px-24">
+        <div className="bg-rose-500 relative flex-1 h-[500px]">
+          <Image src="/images/register.svg" alt="register" fill priority />
+        </div>
+        <div className="bg-cyan-500 flex-1">
+          <RegisterForm />
+        </div>
+      </div>
     </>
   );
 }
--- i-bike/src/components/RegisterForm.jsx
+++ i-bike/src/components/RegisterForm.jsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 export default function RegisterForm() {
   return (
-    <form action="">
-      <input type="text" placeholder="Username" name="username" />
-      <input type="email" placeholder="Email" name="email" />
-      <input type="password" placeholder="Password" name="password" />
-      <input type="password" placeholder="Confirm password" name="passwordRepeat" />
+    <form action="" className="flex flex-col gap-5">
+      <input type="text" placeholder="Username" name="username" className="bg-grey-50 px-4 py-3 rounded-md border border-transparent placeholder-gray-500 focus:outline-none focus:border-transparent" />
+      <input type="email" placeholder="Email" name="email" className="bg-grey-50 px-4 py-3 rounded-md border border-transparent placeholder-gray-500 focus:outline-none focus:border-transparent" />
+      <input type="password" placeholder="Password" name="password" className="bg-grey-50 px-4 py-3 rounded-md border border-transparent placeholder-gray-500 focus:outline-none focus:border-transparent" />
+      <input type="password" placeholder="Confirm password" name="passwordRepeat" className="bg-grey-50 px-4 py-3 rounded-md border border-transparent placeholder-gray-500 focus:outline-none focus:border-transparent" />
+      <button>Login</button>
       {/* <ButtonRegister /> */}
       {/* {state?.error && <p>{state?.error}</p>} */}
       <Link href="/login">
